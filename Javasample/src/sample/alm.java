15576d5aa0ccba2e5f08d24fd097ce679b7170de
GurunathamCH <57170087+GurunathamCH@users.noreply.github.com>
2024-10-15 03:09:38 +0530

    two changes one class added ,changes in another class

--- Javasample/src/sample/alm.java
+++ Javasample/src/sample/alm.java
@@ -1,11 +1,10 @@
 package sample;
 
-public class aa {
+public class alm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi");
-System.out.println("hello");
+
 	}
 
 }
